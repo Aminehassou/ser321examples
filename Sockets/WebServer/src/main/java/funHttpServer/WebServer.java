031c14e1d86fabcebc8a62fd85a145bc161c55df
Amine Hassou <japnoo@gmail.com>
2022-10-24 00:52:50 +0100

    extra fix

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -204,7 +204,7 @@ class WebServer {
           query_pairs = splitQuery(request.replace("multiply?", ""));
 
           // extract required fields from parameters
-          if (query_pairs.get("num1") == null || query_pairs.get("num2") == null) {
+          if (query_pairs.get("num1").equals("") || query_pairs.get("num2").equals("")) {
             builder.append("HTTP/1.1 400 Bad Request\n");
             builder.append("Content-Type: text/html; charset=utf-8\n");
             builder.append("\n");
